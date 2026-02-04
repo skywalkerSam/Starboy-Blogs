024811c17a9a9d4bd8fe134a9f334383dace7866
Starboy <samskywalker001@gmail.com>
2026-02-04 15:28:36 +0600

    minor tweaks

--- src/app/_components/post-body.tsx
+++ src/app/_components/post-body.tsx
@@ -14,21 +14,15 @@ export function PostBody({ content }: Props) {
   const parsedContent = md.render(content ?? "");
 
   return (
-    <section className="font-paprika flex items-center justify-center">
+    <section className="flex items-center justify-center">
       {parsedContent ? (
         <article
-          className="prose mx-auto max-w-xl break-all lg:max-w-3xl md:tracking-wide xl:max-w-4xl"
+          className="prose mx-auto max-w-xl break-all lg:max-w-3xl xl:max-w-4xl"
           dangerouslySetInnerHTML={{ __html: parsedContent }}
         />
       ) : (
         <p>Blogpost coming soon...(</p>
       )}
     </section>
-    // <div className="mx-auto max-w-3xl md:tracking-wide lg:max-w-4xl lg:text-xl xl:max-w-5xl">
-    //   <div
-    //     // className={markdownStyles["markdownStyles.markdown"]}
-    //     dangerouslySetInnerHTML={{ __html: content }}
-    //   />
-    // </div>
   );
 }
